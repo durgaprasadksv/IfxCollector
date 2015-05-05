f8fca624fa237a51b7c51b9008710e375ab7c033
Ubuntu <ubuntu@ip-172-31-16-41.ec2.internal>
2015-05-05 23:50:18 +0000

    matcher

--- collectors/lib/procmon/matcher.py
+++ collectors/lib/procmon/matcher.py
@@ -177,7 +177,7 @@ class SubtreeMatcher:
 		# task_1430862599618_0011_r_000000 task_1430862599618_0011_r_000000_0 
 	        task_id = re.findall('attempt_.*', pinfo.cmd.replace('\x00', ''))[0].strip().split(' ')[0].strip()
 		task_id = task_id.replace('attempt', 'task')
-		print task_id
+		task_id = re.sub(r'_\d+$','', task_id)
 		if '_r_' in pinfo.cmd:
 		    #its a reducer. Change it to above in future
 		    # get the REDUCE_INPUT_RECORDS
